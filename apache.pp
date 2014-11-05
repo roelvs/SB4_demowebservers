def616d2d29ef6ea707ae9686e85bc47bf0c2740
roel van steenberghe <roel.vansteenberghe@kahosl.be>
2014-11-05 14:52:04 +0100

    add firewall rule for apache

--- apache.pp
+++ apache.pp
@@ -5,4 +5,11 @@ apache::vhost {'site1':
 	port => 8000,
 	docroot => '/var/www/personal'
 	}
+	
+firewall { '100 allow apache on port 8000': 
+    port   => [8000],
+    proto  => tcp,
+    action => accept,
+  }
+
 
